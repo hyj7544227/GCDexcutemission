b94e7ce58634e422a440a5f4ec7fb42b0a0406cb
hyj7544227 <3270740558@qq.com>
2017-10-30 17:38:11 +0800

    add gcdintroduce

--- GCDOperationQueue/ViewController.m
+++ GCDOperationQueue/ViewController.m
@@ -17,6 +17,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor yellowColor];
+    [self OperationQueueExcute];
+    
+}
+-(void)OperationQueueExcute{
+    //串行队列
+    dispatch_queue_t serial = dispatch_queue_create("com.serial.gcd", DISPATCH_QUEUE_SERIAL);
+    //并行队列
+    dispatch_queue_t concurrent = dispatch_queue_create("com.concurrent.gcd", DISPATCH_QUEUE_CONCURRENT);
+    //全局队列(并发)
+    dispatch_queue_t global = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    //主队列
+    dispatch_queue_t mainqueue = dispatch_get_main_queue();
+    
+    dispatch_semaphore_t sem =dispatch_semaphore_create(0);
+    
+    
 }
 
 
